ff0bfcd31b3b4627e09a411669811901c325e377
rkhademul8 <rkhademul8.com>
2022-04-22 22:56:51 +0600

    dashboard_setup_done

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import Register from './components/Login/Register/Register';
 import AuthProvider from './context/AuthProvider/AuthProvider';
 import About from './components/About/About';
 import PrivateRoute from './components/Login/PrivateRoute/PrivateRoute';
+import Dashboard from './components/Dashboard/Dashboard/Dashboard';
+import AddProducr from './components/Dashboard/AddProducr/AddProducr';
+import Payment from './components/Dashboard/Payment/Payment';
 function App() {
   return (
     <div className="App">
@@ -21,6 +24,13 @@ function App() {
     <BrowserRouter>
     <Navigation></Navigation>
       <Routes>
+
+          <Route path='/dashboard' element={ <Dashboard />} >
+           
+            <Route path='/dashboard/addProduct' element={<AddProducr />} />
+            <Route path='/dashboard/payment' element={<Payment />} />
+          </Route>
+
           <Route exact path='/'  element={<Home />} />
           <Route path='/home'  element={<Home />} />
           <Route path='/about'  element={<PrivateRoute><About /></PrivateRoute>} />
